ec40d5ada47ce919079d60000c150f94f58918e9
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-07-21 22:56:43 +0600

    Complited backend part-6

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,8 @@ import { userRoutes } from "../modules/user/user.route";
 import { authRoutes } from "../modules/auth/auth.route";
 import { DivisionRoutes } from "../modules/division/division.route";
 import { TourRoutes } from "../modules/tour/tour.route";
+import { BookingRoutes } from "../modules/booking/booking.route";
+import { PaymentRoutes } from "../modules/payment/payment.route";
 
 export const router = Router();
 
@@ -22,10 +24,20 @@ const moduleRoutes = [
     route: DivisionRoutes,
   },
 
-   {
-        path: "/tour",
-        route: TourRoutes
-    }
+  {
+    path: "/tour",
+    route: TourRoutes,
+  },
+
+  {
+    path: "/booking",
+    route: BookingRoutes,
+  },
+
+  {
+    path: "/payment",
+    route: PaymentRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => {
